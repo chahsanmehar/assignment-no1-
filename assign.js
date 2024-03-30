338d0880c4ecb5803141b8419e26962879e67232
chahsanmehar <165464111+chahsanmehar@users.noreply.github.com>
2024-03-30 22:18:56 +0500

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,37 @@
+"use strict";
+// caluclate the area of rectangle
+var height = 10;
+var width = 5;
+var area = height * width;
+console.log(area);
+//caluclate area of circle
+var radius = 5;
+var pi = 3.14;
+var area = Math.PI * radius * radius;
+console.log(area);
+// caluclate area of cube
+var length = 5;
+var area = length * length * length;
+console.log(area);
+// caluclate the persentage of marks
+var marks = 1075;
+var totalmarks = 1100;
+var percentage = marks / totalmarks * 100;
+console.log(percentage, "%");
+// covert mintues into sec
+var minutes = 30;
+var seconds = minutes * 60;
+console.log(seconds, 'sec');
+// convert seconds into mintues
+var seconds = 30;
+var minutes = seconds / 60;
+console.log(minutes, 'min');
+// calculate age  in years
+var currentyear = 2024;
+var age = 2024 - 2005;
+console.log(age, "Year");
+// convert celsius to farnhit temptrue
+var celsius = 37;
+var fahrenheit = celsius * 9 / 5 + 32;
+console.log(fahrenheit, "F");
+// convert farenheit to celsius
